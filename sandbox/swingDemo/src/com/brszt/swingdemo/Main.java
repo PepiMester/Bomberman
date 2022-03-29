4c655ccf1ac27ec62cb87a99417f1d0401cd9796
Weisz Pál <weiszpali@gmail.com>
2022-03-29 13:49:18 +0200

    Add almost empty swing GUI demo

---
+++ sandbox/swingDemo/src/com/brszt/swingdemo/Main.java
@@ -0,0 +1,15 @@
+package com.brszt.swingdemo;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("My First GUI");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300,300);
+        JButton button1 = new JButton("Press");
+        frame.getContentPane().add(button1);
+        frame.setVisible(true);
+    }
+}

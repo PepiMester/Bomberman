6afa65eb31d77cb81d10e46e200fd78ae912c94b
PepiMester <petro.peter.level@gmail.com>
2022-05-08 16:35:40 +0200

    GameWindow

--- Bomberman_v2/src/Bomberman/Map.java
+++ Bomberman_v2/src/Bomberman/Map.java
@@ -1,4 +1,43 @@
 package Bomberman;
 
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
 public class Map {
+
+    public ArrayList<Explosion> Explosions;
+    public ArrayList<Player> Players;
+    public ArrayList<Bomb> Bombs;
+    public ArrayList<Powerup> Powerups;
+    public ArrayList<Obstacle> Obstacles;
+    public ArrayList<BufferedImage> Sprites;
+
+    private void readSprites() {
+
+
+
+
+    }
+    public Map() {
+
+        //Read Sprites
+        readSprites();
+
+        //Create Players in fixed starting positions
+
+        //Create 15x15 map
+
+    }
+
+    public void Update () {
+
+    }
+
+
+
 }
+
+
--- Bomberman_v2/src/Model/Main.java
+++ Bomberman_v2/src/Model/Main.java
@@ -1,4 +1,6 @@
 package Model;
+import javax.swing.*;
+import java.awt.*;
 
 import Bomberman.Map;
 
@@ -6,12 +8,19 @@ import java.time.Clock;
 
 public class Main {
         Double GeneralTime;
-        Bomberman.Map Map;
+        Map Map = new Map();
         Network.Client Client;
         Network.Server Server;
 
         public static void main(String[] args) {
 
-                System.out.println("HELLOBELLO");
+                //A játék egyetlen ablakának létrehozása
+                //System.out.println("HELLOBELLO");
+                JFrame gw = new JFrame("Bomberman");
+                gw.setVisible(true);
+                gw.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);     //így lehet bezárni az ablakot X-szel
+                gw.setSize(15*32,15*32);
+
+
         }
 }

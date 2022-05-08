656315ece0c689829969335419119888db8e8e25
Weisz Pál <weiszpali@gmail.com>
2022-05-08 22:14:37 +0200

    Add map drawing

--- Bomberman_v2/src/Bomberman/Map.java
+++ Bomberman_v2/src/Bomberman/Map.java
@@ -5,83 +5,76 @@ import java.io.IOException;
 import java.util.ArrayList;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
 import javax.imageio.ImageIO;
-import java.io.IOException;
 import java.io.File;
 import java.util.HashMap;
 
-public class Map {
+public class Map extends JFrame {
 
     public ArrayList<Explosion> Explosions;
-    public ArrayList<Player> Players;
-    public ArrayList<Bomb> Bombs;
-    public ArrayList<Powerup> Powerups;
-    public ArrayList<Obstacle> Obstacles;
-    public HashMap<String, BufferedImage> Sprites;
+    public ArrayList<Player> Players = new ArrayList<Player>();
+    public ArrayList<Bomb> Bombs = new ArrayList<Bomb>();
+    public ArrayList<Powerup> Powerups = new ArrayList<Powerup>();
+    public ArrayList<Obstacle> Obstacles = new ArrayList<Obstacle>();
+    public HashMap<String, BufferedImage> Sprites = new HashMap<String, BufferedImage>();
 
-    public enum Images {
-        Background,
-        Unbreakable_Obstacle,
-        Breakable_Obstacle,
-        Powerup_Range,
-        Powerup_Speed,
-        Power_MaxBomb,
-        Powerup_Pierce,
-        Powerup_HP;
-
-        public BufferedImage Sprite = null;
-    }
+    //konstansok, hogy itt egyszerre lehessen paraméterezni a mapot, ne kelljen mindenhol átírni
+    // ha változtatni akarunk esetleg rajta
+    private final int map_width = 15;
+    private final int map_height = 15;
 
     public void readSprites() {
 
-
         try {
-            Sprites.put("UnbreakableObstacle", ImageIO.read(new File("/Sprites/obstacle.png")));
-            //Graphics g = new Graphics();
-
-
+            Sprites.put("UnbreakableObstacle", ImageIO.read(new File("./src/Sprites/wall.png")));
+            Sprites.put("BreakableObstacle", ImageIO.read(new File("./src/Sprites/obstacle.png")));
+            Sprites.put("Player1_sprites", ImageIO.read(new File("./src/Sprites/player1.png")));
+            Sprites.put("Player2_sprites", ImageIO.read(new File("./src/Sprites/player2.png")));
         }
-
         catch (IOException e) {
             System.err.println("Nem sikerült beolvasni a képet");
         }
-
-
     }
     public Map() {
+
+        //JFrame ősosztály inicializálása
+        super("Bomberman");
 
         //Sprite-ok beolvasása
         readSprites();
 
         //játékosok lerakása a pálya átelennes sarkaiba
-        //TODO
-        //Players.add(new Player(Sprites.get("Player1_sprites", [0,0])));
-        //Players.add(new Player(Sprites.get("Player1_sprites", [0,0])));
+        Players.add(new Player(Sprites.get("Player1_sprites"), new int[] {1, 1}));
+        Players.add(new Player(Sprites.get("Player2_sprites"), new int[] {map_width-2,  map_width-2}));
 
+        //pálya létrehozása
+        for (int i=0; i<map_width; i++) {
+            for (int j=0; j<map_height; j++) {
 
-        //15x15-ös pálya létrehozása
-        for (int i=0; i<15; i++) {
-            for (int j=0; j<15; j++) {
+                int[] position = new int[] {i, j};
 
                 //pálya körbe rakása felrobbanthatatlan akadályokkal
-                if(i==0 || i==14 || j==0 || j==14) {
-                    //Obstacles.add(new Obstacle())
+                if(i==0 || i==map_width-1 || j==0 || j==map_height-1) {
+                    Obstacles.add(new Obstacle(Sprites.get("UnbreakableObstacle"), position, false));
                 }
-
                 //felrobbanthatatlan akadályok sakktábla szerúen előre megadott potícióban
-                if {
-
+                else if((i%2==0) && (j%2==0))
+                {
+                    Obstacles.add(new Obstacle(Sprites.get("UnbreakableObstacle"), position, false));
                 }
-
-                //felrobbantható akadályok véletlenszerű esállyel, de a játékoshoz 2 mezőnél nem közelebb
-                //TODO
-
-
+                //felrobbantható akadályok véletlenszerű eséllyel, de a játékoshoz 2 mezőnél nem közelebb
+                else if(!((i==1 && j<4) || (i<4 && j==1) || (i==map_width-2 && j>map_height-5) || (i>map_width-5 && j==map_height-2))){
+                    Obstacles.add(new Obstacle(Sprites.get("BreakableObstacle"), position, true));
+                }
             }
         }
 
-
+        //A játék egyetlen ablakának létrehozása
+        setDefaultCloseOperation(EXIT_ON_CLOSE);     //így lehet bezárni az ablakot X-szel
+        getContentPane().setPreferredSize(new Dimension(map_width * 32 + getInsets().left + getInsets().right, map_height * 32 + getInsets().top + getInsets().bottom));
+        setResizable(false);
+        pack();
+        setVisible(true);
     }
 
     public void Update () {
@@ -89,8 +82,19 @@ public class Map {
         BufferedImage img = Sprites.get("UnbreakableObstacle");
     }
 
+    @Override
+    public void paint(Graphics g){
+        super.paint(g);
+        g.setColor(Color.gray);
+        g.fillRect(0, 0, map_width*32, map_height*32);
+        for (Obstacle obstacle: Obstacles) {
+            g.drawImage(obstacle.currentSprite, obstacle.position[0]*32+getInsets().left, obstacle.position[1]*32+getInsets().top, 32, 32, null);
+        }
 
-
+        for (Player player: Players) {
+            g.drawImage(player.currentSprite, player.position[0]*32+getInsets().left, player.position[1]*32 + getInsets().top, null);
+        }
+    }
 }
 
 
--- Bomberman_v2/src/Model/Main.java
+++ Bomberman_v2/src/Model/Main.java
@@ -8,22 +8,10 @@ import java.time.Clock;
 
 public class Main {
         Double GeneralTime;
-        Map Map = new Map();
         Network.Client Client;
         Network.Server Server;
 
         public static void main(String[] args) {
-
-                //A játék egyetlen ablakának létrehozása
-                //System.out.println("HELLOBELLO");
-                JFrame gw = new JFrame("Bomberman");
-                gw.setVisible(true);
-                gw.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);     //így lehet bezárni az ablakot X-szel
-                gw.setSize(15*32,15*32);
-
-                //Map.update();
-                //while(1) {
-                  //
-                //}
+                Map Map = new Map();
         }
 }
